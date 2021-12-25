981e7fab5969a4aceb4e565409b717fe26b95528
s20041205 <stlinalice@gmail.com>
2021-12-25 16:44:33 +0800

    adding index.php

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Cabin+Sketch:wght@700&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/e3098ea442.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <title>About Me</title>
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="navbar-brand" href="#">
+                        <!--<img src="pictures/ele.logo.gif">-->
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.html">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Profile</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Portfolios
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="bookkeeping/index.html">Bookkeeping</a>
+                                    <a class="dropdown-item disabled" href="#">Leave System</a>
+                                    <a class="dropdown-item" href="bootstrap/start/index.html">Company Intro</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item disabled" href="#">Others</a>
+                                </div>
+                            </li>
+                        </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                    </div>
+                </nav>
+            </div>
+        </header>
+        <section id="intro">
+            <div class="jumbotron">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h1>About Me</h1>
+                            <p class="lead">Software engineer with 4 years of experience in the arena of medical software system</p>
+                            
+                            <!--<a class='btn' href='#'>Enter</a>-->
+                            <button type="button" class="btn btn-secondary">Enter</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>   
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class='col-md-5 text-left'>
+                        <h4>Link</h4>
+                        <a href='index.html'>首頁</a>
+                        <li><a href='http://www.saihs.edu.tw/nss/p/index'>松山工農</a></li>					  
+                    </div>
+                    <div class='col-md-5 text-left'>
+                        <h4>臺北市立松山高級工農職業學校‧電子科</h4>
+                        <p>11060臺北市信義區忠孝東路5段236巷15號<br>
+                        <i class="fas fa-phone-alt"></i>  (02)27226616 #531<br>
+                        <i class="far fa-envelope-open"></i> <a href='mailto:ele@saihs.edu.tw'>ele@saihs.edu.tw</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
