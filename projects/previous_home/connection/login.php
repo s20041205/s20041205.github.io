4bff951dbc9615de017752c23d832e6cf1f0277e
Jerry_Hu <g910414@hotmail.com>
2026-05-01 00:20:07 +0800

    Refine unused files
    Signed-off-by: Jerry_Hu <g910414@hotmail.com>

--- projects/previous_home/connection/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-           $dbhost = 'https://192-168-0-139.hcclst.direct.quickconnect.to/';  //server
-           $dbuser = 'root';       //user
-           $dbpass = '*AeAc0805*';   //pw
-           $dbname = 'hcclst_home';     //db name
-           $conn = mysql_connect($dbhost, $dbuser, $dbpass) or die('Error with MySQL connect') ;
-           mysql_query("SET NAMES 'UTF8'"); 
-           mysql_select_db($dbname);
-?>
