bac9723cd7d358921eff4fc0755d19d283468d03
g910414 <g910414@users.noreply.github.com>
2026-05-02 15:50:39 +0000

    deploy: 3fec96df21f98e6e987f27194f5602cf619bf137

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const APP_CONFIG = {
     scriptWriteUrl: 'https://script.google.com/macros/s/AKfycbyi5SJK4X_O4GNBLzbH6ftMP0gYgEc_AvY-jGmVnf0grfgeUb33GGBidbEzKCXERBl4/exec',
-    scriptReadUrl:  'https://script.google.com/macros/s/AKfycbx6bHF-dqbT0psOR5CAXiO2uiU8Om3ScTZ0ei9HCCEbeVz5AbCugCzY30cMca1cOSst/exec'
+    scriptReadUrl:  'https://script.google.com/macros/s/AKfycbx6bHF-dqbT0psOR5CAXiO2uiU8Om3ScTZ0ei9HCCEbeVz5AbCugCzY30cMca1cOSst/exec',
+    sheetUrl:       'https://docs.google.com/spreadsheets/d/1ZNPbB2IAaM23TGnwfXr4bTbijXXBPwMMNTZa_O5ERhE/edit?usp=sharing'
 };
